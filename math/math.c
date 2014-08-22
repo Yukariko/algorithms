eefa2f56ce8ff98e76835eb1a6c8531c713f4e80
Yukariko <rkdals7979@naver.com>
2014-08-22 10:38:39 +0900

    add Big Plus

--- math/math.c
+++ math/math.c
@@ -20,11 +20,27 @@ eratos(int *arr,int size,int n)
       arr[j]=1;
   }
 }
-
 primeFactorCountOfFactorial(n,i)
 {
   int result=0,j;
   for(j=i;j<=n;j*=i)result+=n/j;
   return result;
+}
+bigplus(char *store,char *a,char *b)
+{
+  char t[1001]={0};
+  int i=strlen(a),j=strlen(b),p,s;
+  for(p=0;i||j;p++)
+  {
+    s = (i?a[i-1]:'0')+(j?b[j-1]:'0')-'0'*2;
+    if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
+    t[p] = (s+t[p])%10;
+    if(i>0)i--;
+    if(j>0)j--;
+  }
+  if(!t[p])p--;
+  for(i=0;i<=p;i++)
+   store[p-i]=t[i]+'0';
+  store[p+1]=0;
 }
 main(){}

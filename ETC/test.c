de1c5560fea0a9c36858c38c88f002bb596754c1
Yukariko <rkdals7979@naver.com>
2014-09-17 20:43:49 +0900

    ETC
    add digitCount

--- ETC/test.c
+++ ETC/test.c
@@ -1,3 +1,24 @@
+long long digitCount(long long n,int i)
+{
+  long long res,j,k,t;
+  res=0;
+  for(t=0,j=1;j<=n;j*=10,t++)
+  {
+    k=(n/j)%10;
+    if(i==0)
+    {
+      if(!k)
+      {
+        res-=j-1;
+        res+=n%j;
+      }
+    }
+    else if(i<k)res+=j;
+    else if(i==k)res+=n%j+1;
+    res+=k*t*(j/10);
+  }
+  return res;
+}
 putn(n,m,p)
 {
   return n-(n/p%10)*p+m*p;

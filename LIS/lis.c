533c844b9b7ba02e293c2a765acaf36c4c7e5020
Yukariko <rkdals7979@naver.com>
2015-01-27 17:41:11 +0900

    LIS
    add) find component

--- LIS/lis.c
+++ LIS/lis.c
@@ -1,32 +1,43 @@
-binary_search(int *arr,int size,int num)
+binary_search(int *arr,int *orgn,int size,int num)
 {
    int start=0,end=size-1,mid;
    while(start<=end)
    {
      mid=(end+start)/2;
-     if(num>arr[mid])start=mid+1;
-     else if(num<arr[mid])end=mid-1;
+     if(num>orgn[arr[mid]])start=mid+1;
+     else if(num<orgn[arr[mid]])end=mid-1;
      else return mid;
    }
    return start;
 }
-LIS(int *arr,int size)
+LIS(int *arr,int size,int *lis)
 {
   int *b=(int *)malloc(size*4);
+  int *parent = (int *)malloc(size*4);
+  memset(parent,-1,size*4);
   int i,j,k=0;
   for(i=0;i<size;i++)
   {
-    j=binary_search(b,k,arr[i]);
+    j=binary_search(b,arr,k,arr[i]);
     if(j==k)
     {
-      b[k++]=arr[i];
+      b[k++]=i;
+      if(k>1)parent[i]=b[k-2];
     }
     else
     {
-      b[j]=arr[i];
+      b[j]=i;
+      if(j)parent[i]=b[j-1];
     }
   }
+  int child=b[k-1];
+  for(i=k-1;child!=-1;i--)
+  {
+  	lis[i]=arr[child];
+  	child=parent[child];
+  }
   free(b);
+  free(parent);
   return k;
 }
 main(){}

b61fa47984e3292dd3db7acb0f46f3c72aba0127
Yukariko <rkdals7979@naver.com>
2015-08-24 15:54:06 +0900

    MODEXP

--- modular_exponentiation/test.c
+++ modular_exponentiation/test.c
@@ -1,14 +1,14 @@
 //a^b mod c
 long long mod_exp(long long a,long long b,long long c)
 {
-  a%=c;
-  if(b==0)return 1;
-  if(b==1)return a;
-  if(b&1) return (a*mod_exp((a*a)%c,(b-1)/2,c))%c;
-  return mod_exp((a*a)%c,b/2,c);
+	a%=c;
+	if(b==0)return 1;
+	if(b==1)return a;
+	if(b&1) return (a*mod_exp((a*a)%c,(b-1)/2,c))%c;
+	return mod_exp((a*a)%c,b/2,c);
 }
 
 main()
 {
-  printf("%lld",mod_exp(2,62,1LL<<63));
+	printf("%lld",mod_exp(2,62,1LL<<63));
 }

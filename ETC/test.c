aa34ea0c66ee116b783e162b09e1633225835e0c
Yukariko <rkdals7979@naver.com>
2015-09-17 17:55:03 +0900

    etc

--- ETC/test.c
+++ ETC/test.c
@@ -1,42 +1,42 @@
 long long digitCount(long long n,int i)
 {
-  long long res,j,k,t;
-  res=0;
-  for(t=0,j=1;j<=n;j*=10,t++)
-  {
-    k=(n/j)%10;
-    if(i==0)
-    {
-      if(!k)
-      {
-        res-=j-1;
-        res+=n%j;
-      }
-    }
-    else if(i<k)res+=j;
-    else if(i==k)res+=n%j+1;
-    res+=k*t*(j/10);
-  }
-  return res;
+	long long res,j,k,t;
+	res=0;
+	for(t=0,j=1;j<=n;j*=10,t++)
+	{
+		k=(n/j)%10;
+		if(i==0)
+		{
+			if(!k)
+			{
+				res-=j-1;
+				res+=n%j;
+			}
+		}
+		else if(i<k)res+=j;
+		else if(i==k)res+=n%j+1;
+		res+=k*t*(j/10);
+	}
+	return res;
 }
-putn(n,m,p)
+int putn(n,m,p)
 {
-  return n-(n/p%10)*p+m*p;
+	return n-(n/p%10)*p+m*p;
 }
-isPalin(char *a,int len)
+int isPalin(char *a,int len)
 {
-  int i=len/2;
-  int p,q;
-  if(len%2==0)
-  {
-    p=i-1;q=i;
-    for(;p>=0&&q<len;p--,q++)if(a[p]!=a[q])return 0;
-  }
-  else
-  {
-    p=i;q=p;    
-    for(;p>=0&&q<len;p--,q++)if(a[p]!=a[q])return 0;
-  }
-  return 1;
+	int i=len/2;
+	int p,q;
+	if(len%2==0)
+	{
+		p=i-1;q=i;
+		for(;p>=0&&q<len;p--,q++)if(a[p]!=a[q])return 0;
+	}
+	else
+	{
+		p=i;q=p;    
+		for(;p>=0&&q<len;p--,q++)if(a[p]!=a[q])return 0;
+	}
+	return 1;
 }
 main(){}

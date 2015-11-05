f36114261ae7f3cb608373fd80fa1c94a6b7c5d6
Yukariko <rkdals7979@naver.com>
2015-11-05 20:15:54 +0900

    test

--- test.cpp
+++ test.cpp
@@ -81,3 +81,15 @@ int kruskal(vector<pair<int,int>>& selected)
 	}
 	return ret;
 }
+
+long long extgcd(long long a, long long b, long long* x, long long* y){
+    long long d = a;
+    if(b != 0){
+        d = extgcd(b, a % b, y, x);
+        (*y) -= (a/b) * (*x);
+    }
+    else{
+        (*x)=1, (*y)=0;
+    }
+    return d;
+}

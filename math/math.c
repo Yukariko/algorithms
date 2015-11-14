3cb1c11bebdd9934f0af231d0d5eb2206aeaed2b
Yukariko <rkdals7979@naver.com>
2015-11-14 15:41:03 +0900

    math

--- math/math.c
+++ math/math.c
@@ -1,65 +1,65 @@
 isPrime(p)
 {
-  int i,k=sqrt(p)+1e-6;
-  //for(i=2;i*i<=p;i++)
-  for(i=2;i<=k;i++)
-  {
-    if(p%i==0)return 0;
-  }
-  return 1;
+	int i,k=sqrt(p)+1e-6;
+	//for(i=2;i*i<=p;i++)
+	for(i=2;i<=k;i++)
+	{
+		if(p%i==0)return 0;
+	}
+	return 1;
 }
 
 gcd(a,b){return b?gcd(b,a%b):a;}
 
 eratos(int *arr,int size,int n)
 {
-  int i,j;
-  for(i=2;i<=n;i++)
-  {
-    for(j=i*i;j<=n;j+=i)
-      arr[j]=1;
-  }
+	int i,j;
+	for(i=2;i<=n;i++)
+	{
+		for(j=i*i;j<=n;j+=i)
+			arr[j]=1;
+	}
 }
 primeFactorCountOfFactorial(n,i)
 {
-  int result=0,j;
-  for(j=i;j<=n;j*=i)result+=n/j;
-  return result;
+	int result=0,j;
+	for(j=i;j<=n;j*=i)result+=n/j;
+	return result;
 }
 char *bigplus(char *store,char *a,char *b)
 {
-  char t[1001]={0};
-  int i=strlen(a),j=strlen(b),p,s;
-  for(p=0;i||j;p++)
-  {
-    s = (i?a[i-1]-'0':0)+(j?b[j-1]-'0':0);
-    if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
-    t[p] = (s+t[p])%10;
-    if(i>0)i--;
-    if(j>0)j--;
-  }
-  if(!t[p])p--;
-  for(i=0;i<=p;i++)
-   store[p-i]=t[i]+'0';
-  store[p+1]=0;
-  return store;
+	char t[1001]={0};
+	int i=strlen(a),j=strlen(b),p,s;
+	for(p=0;i||j;p++)
+	{
+		s = (i?a[i-1]-'0':0)+(j?b[j-1]-'0':0);
+		if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
+		t[p] = (s+t[p])%10;
+		if(i>0)i--;
+		if(j>0)j--;
+	}
+	if(!t[p])p--;
+	for(i=0;i<=p;i++)
+	 store[p-i]=t[i]+'0';
+	store[p+1]=0;
+	return store;
 }
 char *bigMulti(char *store,char *a,char *b)
 {
-  char t[1001]={0};
-  int i=strlen(a),j=strlen(b),p,s;
-  for(p=0;i||j;p++)
-  {
-    s = (i?a[i-1]-'0':0)*(j?b[j-1]-'0':0);
-    if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
-    t[p] = (s+t[p])%10;
-    if(i>0)i--;
-    if(j>0)j--;
-  }
-  if(!t[p])p--;
-  for(i=0;i<=p;i++)
-   store[p-i]=t[i]+'0';
-  store[p+1]=0;
-  return store;
+	char t[1001]={0};
+	int i=strlen(a),j=strlen(b),p,s;
+	for(p=0;i||j;p++)
+	{
+		s = (i?a[i-1]-'0':0)*(j?b[j-1]-'0':0);
+		if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
+		t[p] = (s+t[p])%10;
+		if(i>0)i--;
+		if(j>0)j--;
+	}
+	if(!t[p])p--;
+	for(i=0;i<=p;i++)
+	 store[p-i]=t[i]+'0';
+	store[p+1]=0;
+	return store;
 }
 main(){}
